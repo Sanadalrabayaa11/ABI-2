8b1e7bdcec516be4e679ef84817d8523137265d9
sanadalrabayaa11 <sanadhayajneh11@gmail.com>
2024-02-11 23:12:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+async function getProduct() {
+  const response = await axios.get("https://dummyjson.com/products");
+  const data = response.data.products;
+  const result = data
+    .map(function (e) {
+      return `
+      <div class='product'>
+      <h2>${e.title}</h2>
+      <img src='${e.thumbnail}'/>
+      <a href="details.html?product_id=${e.id}">DETAILS</a>
+      </div>
+      `;
+    })
+    .join("");
+  document.querySelector(".home").innerHTML = result;
+}
+getProduct();
